eb73c2a543c213bdeb26d7daa4bcf423d28fc12b
barberj <barber.justin@gmail.com>
2014-12-19 07:58:53 -0500

    request stub

--- app/models/insightly.rb
+++ app/models/insightly.rb
@@ -1,2 +1,10 @@
 class Insightly < Api
+  def request(resource, params={})
+    #HTTParty.get("https://api.insight.ly/v2.1/#{resource}",
+    #  :query   => params,
+    #  :headers => {
+    #    'Authorization' => "Basic #{Base64.encode64(config[:api_key]).chomp}"
+    #  }
+    #)
+  end
 end
