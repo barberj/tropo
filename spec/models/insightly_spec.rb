cbe8b9abbd10a8d5a3c3bcd361c09f6af2fc1d05
barberj <barber.justin@gmail.com>
2014-12-19 08:33:58 -0500

    encrypted_data specs

--- spec/models/insightly_spec.rb
+++ spec/models/insightly_spec.rb
@@ -14,7 +14,21 @@ describe Insightly do
     expect(Api.find_by_token('insightly_token')).to eq(insightly)
   end
 
-  it '#data' do
-    expect(insightly.data[:api_key]).to eq('letmein')
+  describe '.encrypted_data' do
+    it '#data returns decrypted client data' do
+      expect(insightly.data[:api_key]).to eq('letmein')
+    end
+    it '#data! updates encrypted client data' do
+      insightly.data!(key_two: 'two')
+
+      expect(insightly.data[:key_two]).to eq('two')
+      expect(insightly.data[:api_key]).to eq('letmein')
+    end
+    it '#data sets encrypted client data' do
+      insightly.data= {key_two: 'two'}
+
+      expect(insightly.data[:key_two]).to eq('two')
+      expect(insightly.data[:api_key]).to be_nil
+    end
   end
 end
