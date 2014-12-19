6019b4d096dcb0082a3550c216b32157b2b57649
barberj <barber.justin@gmail.com>
2014-12-19 08:36:16 -0500

    move more generic api specs to api_spec

--- spec/models/api_spec.rb
+++ spec/models/api_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 describe Api do
-  pending 'specs'
+  let!(:api) do
+    id = create(:api, :data => {:api_key => 'letmein'}).id
+    Api.find(id)
+  end
+
+  it '#name defaults to type' do
+    expect(api.name).to eq('Insightly')
+  end
+
+  it '#find_by_token' do
+    expect(Api.find_by_token('insightly_token')).to eq(api)
+  end
+
+  describe '.encrypted_data' do
+    it '#data returns decrypted client data' do
+      expect(api.data[:api_key]).to eq('letmein')
+    end
+    it '#data! updates encrypted client data' do
+      api.data!(key_two: 'two')
+
+      expect(api.data[:key_two]).to eq('two')
+      expect(api.data[:api_key]).to eq('letmein')
+    end
+    it '#data sets encrypted client data' do
+      api.data= {key_two: 'two'}
+
+      expect(api.data[:key_two]).to eq('two')
+      expect(api.data[:api_key]).to be_nil
+    end
+  end
 end
--- spec/models/insightly_spec.rb
+++ spec/models/insightly_spec.rb
@@ -1,34 +1,4 @@
 require 'rails_helper'
 
 describe Insightly do
-  let!(:insightly) do
-    id = create(:api, :data => {:api_key => 'letmein'}).id
-    Api.find(id)
-  end
-
-  it '#name defaults to type' do
-    expect(insightly.name).to eq('Insightly')
-  end
-
-  it '#find_by_token' do
-    expect(Api.find_by_token('insightly_token')).to eq(insightly)
-  end
-
-  describe '.encrypted_data' do
-    it '#data returns decrypted client data' do
-      expect(insightly.data[:api_key]).to eq('letmein')
-    end
-    it '#data! updates encrypted client data' do
-      insightly.data!(key_two: 'two')
-
-      expect(insightly.data[:key_two]).to eq('two')
-      expect(insightly.data[:api_key]).to eq('letmein')
-    end
-    it '#data sets encrypted client data' do
-      insightly.data= {key_two: 'two'}
-
-      expect(insightly.data[:key_two]).to eq('two')
-      expect(insightly.data[:api_key]).to be_nil
-    end
-  end
 end
