3ae7202b8c1d5f89d607a4de58bd3be63b07e0af
barberj <barber.justin@gmail.com>
2014-12-29 20:29:46 -0500

    use path vs hardcoding url

--- spec/requests/api/v1/api/authorization_spec.rb
+++ spec/requests/api/v1/api/authorization_spec.rb
@@ -3,37 +3,37 @@ require 'rails_helper'
 describe 'Api Request' do
   context 'with no Authorization Header' do
     it 'get returns 401' do
-      expect(get("/api/v1/contacts'")).to eq 401
+      expect(get(api_v1_path('contacts'))).to eq 401
     end
     it 'post returns 401' do
-      expect(post("/api/v1/contacts'")).to eq 401
+      expect(post(api_v1_path('contacts'))).to eq 401
     end
     it 'put returns 401' do
-      expect(put("/api/v1/contacts'")).to eq 401
+      expect(put(api_v1_path('contacts'))).to eq 401
     end
     it 'delete returns 401' do
-      expect(delete("/api/v1/contacts'")).to eq 401
+      expect(delete(api_v1_path('contacts'))).to eq 401
     end
   end
 
   context 'with bad Authorization Header' do
     it 'get returns 401' do
-      expect(get("/api/v1/contacts'",
+      expect(get(api_v1_path('contacts'),
         'HTTP_AUTHORIZATION' => "Token bad_token"
       )).to eq 401
     end
     it 'post returns 401' do
-      expect(post("/api/v1/contacts",
+      expect(post(api_v1_path('contacts'),
         'HTTP_AUTHORIZATION' => "Token bad_token"
       )).to eq 401
     end
     it 'put returns 401' do
-      expect(put("/api/v1/contacts",
+      expect(put(api_v1_path('contacts'),
         'HTTP_AUTHORIZATION' => "Token bad_token"
       )).to eq 401
     end
     it 'delete returns 401' do
-      expect(delete("/api/v1/contacts",
+      expect(delete(api_v1_path('contacts'),
         'HTTP_AUTHORIZATION' => "Token bad_token"
       )).to eq 401
     end
