f401957911035710e74e8d8b92deae8dc93b92bf
barberj <barber.justin@gmail.com>
2015-01-15 16:43:48 -0500

    redirect back to proxy

--- app/controllers/insightly_controller.rb
+++ app/controllers/insightly_controller.rb
@@ -4,10 +4,10 @@ class InsightlyController < ApiController
       token: session['token'],
       data: { api_key: params['key'] }
     )
-    if api.valid?
-      redirect_to '/'
-    else
+    if !api.valid?
       redirect_to :back
+    else
+      redirect_to 'http://127.0.0.1:3000/dashboard'
     end
   end
 end
